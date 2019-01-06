8c4a6ba141aa63658993aa22fda6f54057619996
jeffplata@yahoo.com <jeffplata@yahoo.com>
2019-01-06 22:18:43 +0800

    first commit

---
+++ models/menu.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+# ----------------------------------------------------------------------------------------------------------------------
+# this is the main application menu add/remove items as required
+# ----------------------------------------------------------------------------------------------------------------------
+
+response.menu = [
+    (T('Home'), False, URL('default', 'index'), [])
+]
+
+# ----------------------------------------------------------------------------------------------------------------------
+# provide shortcuts for development. you can remove everything below in production
+# ----------------------------------------------------------------------------------------------------------------------
+
+response.menu += [
+    (T('Services'), False, URL('default', 'list_services'), []),
+    (T('Members'), False, URL('default', 'index'), []),
+    (T('Loans'), False, URL('default', 'index'), []),
+    (T('Design'), False, 'http://127.0.0.1:8000/admin/default/design/LoansManager', []),
+    ]
